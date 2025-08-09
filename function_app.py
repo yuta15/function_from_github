0a0aafc65e530895b192de70b63b0d6bf6036163
yuta15 <yxyi8udm1eb1us6aea7s@gmail.com>
2025-08-09 22:07:36 +0900

    wf修正

--- function_app.py
+++ function_app.py
@@ -1,10 +1,10 @@
 import azure.functions as func
 import logging
 
-# from blueprints.resource_group import BP as resource_group_bp
+from blueprints.resource_group import BP as resource_group_bp
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
-# app.register＿functions（resource＿group＿bp）
+app.register_blueprint(resource_group_bp)
 
 
 @app.route(route="ping", auth_level=func.AuthLevel.ANONYMOUS)
