a5a16787e49e9c5da18f80e99efeba1358baa7f7
yuta15 <70703081+yuta15@users.noreply.github.com>
2025-08-09 21:44:08 +0900

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -4,9 +4,10 @@ import logging
 from blueprints.resource_group import BP as resource_group_bp
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
-app.register_functions(resource_group_bp)
+# app.register＿functions（resource＿group＿bp）
 
 
 @app.route(route="ping", auth_level=func.AuthLevel.ANONYMOUS)
 def ping(req: func.HttpRequest) -> func.HttpResponse:
+
     return func.HttpResponse("pong")
