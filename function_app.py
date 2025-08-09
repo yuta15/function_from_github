7e1ea92c8e1894c79c1c54cfc19ad084a575b496
yuta15 <yxyi8udm1eb1us6aea7s@gmail.com>
2025-08-09 21:17:12 +0900

    __init__.pyを追加

--- function_app.py
+++ function_app.py
@@ -4,4 +4,4 @@ import logging
 from blueprints.resource_group import BP as resource_group_bp
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
-app.register_blueprint(resource_group_bp)
+app.register_functions(resource_group_bp)
